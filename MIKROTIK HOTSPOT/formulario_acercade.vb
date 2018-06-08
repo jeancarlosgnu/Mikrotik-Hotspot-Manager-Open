381cad024558bb37721f7d4f36b980f660c053a1
JEAN CARLOS GOMEZ <jeancarlosgnu@hotmail.com>
2018-06-07 21:33:25 -0500

    .

--- MIKROTIK HOTSPOT/formulario_acercade.vb
+++
@@ -1,7 +0,0 @@
-﻿Public Class formulario_acercade
-
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        'directoriofuentes()
-        Me.Close()
-    End Sub
-End Class
